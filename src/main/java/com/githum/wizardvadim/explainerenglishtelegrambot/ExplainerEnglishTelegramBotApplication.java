a462264b88599b7cc4eae73df418cbf6034bad0f
Vekhov Vadim <106478770+wizardVadim@users.noreply.github.com>
2022-11-26 17:36:01 +0800

    EEnTB-0: Added Spring Boot Skeleton to the project

---
+++ src/main/java/com/githum/wizardvadim/explainerenglishtelegrambot/ExplainerEnglishTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.githum.wizardvadim.explainerenglishtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplainerEnglishTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplainerEnglishTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githum/wizardvadim/explainerenglishtelegrambot/ExplainerEnglishTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githum.wizardvadim.explainerenglishtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplainerEnglishTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
